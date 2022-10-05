ff6837a9125da2461f2db70ed908d42fd7231567
Yassine Tazi <yassine.tazi@ehb.be>
2022-10-05 16:29:50 +0200

    made the space invaders logo

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,17 @@
 "use strict";
 
-alert("Draw your space invader here");
+
+let cnv = document.querySelector("canvas");
+cnv.width = innerWidth;
+cnv.height = innerHeight;
+let ctx = cnv.getContext("2d");
+
+ctx.beginPath();
+ctx.fillStyle = "green"
+ctx.rect(20,150,100,100);
+ctx.rect(500,150,100,100);
+ctx.rect(20,50,580,100);
+ctx.rect(265,450,100,100);
+ctx.rect(265,250,100,100);
+ctx.fill();
+ctx.stroke();
