114a6d3382325bee749a7d711af9285b689e5c3e
Yassine Tazi <yassine.tazi@ehb.be>
2022-10-05 22:33:21 +0200

    changed the size and colors to the correct one

--- spaceInvader.js
+++ spaceInvader.js
@@ -7,11 +7,19 @@ cnv.height = innerHeight;
 let ctx = cnv.getContext("2d");
 
 ctx.beginPath();
-ctx.fillStyle = "green"
-ctx.rect(20,150,100,100);
-ctx.rect(500,150,100,100);
-ctx.rect(20,50,580,100);
-ctx.rect(265,450,100,100);
-ctx.rect(265,250,100,100);
+ctx.fillStyle = "black";
+ctx.rect(0,0,300,300);
 ctx.fill();
-ctx.stroke();
+
+ctx.beginPath();
+ctx.fillStyle = "Lime";
+ctx.rect(20,70,50,50);
+ctx.rect(220,70,50,50);
+ctx.rect(20,20,50,50);
+ctx.rect(70,20,50,50);
+ctx.rect(120,20,50,50);
+ctx.rect(170,20,50,50);
+ctx.rect(220,20,50,50);
+ctx.rect(120,220,50,50);
+ctx.rect(120,120,50,50);
+ctx.fill();
